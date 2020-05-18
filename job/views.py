7d9c52485df4798bb35d6cea89dfbd5549a9ef47
Lee <114220313@umail.ucc.ie>
2020-05-18 21:21:31 +0100

    init

---
+++ job/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from job.models import Job
+
+
+def index(request):
+    return render(request, 'jobs/index.html')
+
+
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
+
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/detail.html', {'job': job_detail})
