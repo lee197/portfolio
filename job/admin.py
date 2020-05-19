295cdefb6476f93d126aa80eeaddaaa9fce73913
Lee <114220313@umail.ucc.ie>
2020-05-20 00:21:07 +0100

    add more info in models

--- job/admin.py
+++ job/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Job
+from .models import Job, Person, Skill, Category
 
 # Register your models here.
 admin.site.register(Job)
+admin.site.register(Person)
+admin.site.register(Skill)
+admin.site.register(Category)
+
