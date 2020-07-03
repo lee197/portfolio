a87afdfcc210524850a019e73207c349962a2396
Lee <114220313@umail.ucc.ie>
2020-07-03 18:35:31 +0100

    change job to projects

--- job/models.py
+++ job/models.py
@@ -74,7 +74,7 @@ class Person(models.Model):
 
 
 # Create your models here.
-class Job(models.Model):
+class Project(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
